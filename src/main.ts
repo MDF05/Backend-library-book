6b3a66bf4ab9139a52055346f0f901196b610d03
muhammad dava fahreza <mdavafahreza05@gmail.com>
2025-04-10 23:52:12 +0700

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,17 @@ import swaggerSpec from "./swagger";
 const app = express();
 app.use(express.json());
 
+
 app.use("/api/books", bookRoutes);
 app.use("/api/members", memberRoutes);
 app.use("/api/borrow", borrowRoutes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/", (req, res) => {
+  res.send(
+    "swagger docs : <a href='/api-docs'>http://localhost:3000/api-docs/</a>"
+  );
+});
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
